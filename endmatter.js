8c9ea9f017e488a5733052c7a5da4c6229d0bdff
Rupert Redington <rupert@neontribe.co.uk>
2014-09-01 22:21:23 +0100

    surely now?

---
+++ endmatter.js
@@ -0,0 +1,14 @@
+/*
+	* node (http://nodejs.org)
+	* gnode (https://github.com/TooTallNate/gnode)
+	* traceur (http://google.github.io/traceur-compiler)
+	* regenerator (http://facebook.github.io/regenerator)
+	* thunkify (https://github.com/visionmedia/node-thunkify)
+	* bluebird (https://github.com/petkaantonov/bluebird)
+	* co (https://github.com/visionmedia/co)
+	* koa (http://koajs.com)
+	* mapit (http://mapit.mysociety.org)
+	* Great British Public Toilet Map (http://gbptm-ui-stage.herokuapp.com)
+	
+	http://github.com/marxian/joy-of-generators
+ */
